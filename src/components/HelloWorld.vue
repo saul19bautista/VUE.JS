d40f72cb1848f0e0c7da4ba6be3c77a0751df719
javiern-778 <79878933+javiern-778@users.noreply.github.com>
2025-03-19 19:46:31 -0600

    se agrego el cards en el diseño de helloword

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,22 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <!-- Estructura HTML -->
-  <div id="particles-js"></div> <!-- Este es el contenedor para las partículas -->
+  <div id="particles-js"></div>
+  <!-- Este es el contenedor para las partículas -->
 
-  <header> 
+  <header>
     <img src="../assets/flor.png" alt="Vue Logo" />
     <div>
       <h1>Hotel Flor del Saber</h1>
       <p>Tu destino de ensueño</p>
     </div>
     <form action="login.html" method="POST">
-      <button class="logout-button" type="submit" name="logout">Cerrar sesión</button>
+      <button class="logout-button" type="submit" name="logout">
+        Cerrar sesión
+      </button>
     </form>
   </header>
-  
 
   <nav>
     <a href="registros_clientes.php">Registrar Cliente</a>
@@ -27,21 +28,62 @@
     <a href="cliente_reservaciones.php">Reservaciones</a>
     <a href="habitaciones.php">Habitaciones</a>
   </nav>
-
+  
   <div class="container">
     <div class="hero-image">
       <h1>Bienvenido a Hotel Flor del Saber donde el privilegio eres tú</h1>
     </div>
 
+    <div class="row mt-5">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://imgs.search.brave.com/MrC3IUGLKbHdv5ng-cIQ6mavCtaHnJ2ncwukVsdSiAE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzYyLzU5Lzcw/LzM2MF9GXzYyNTk3/MDM1X2tKVGtTUFpt/OVAxME9HZnBrV0RY/NWlIbEJ5YXJ1TVVW/LmpwZw" 
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+          <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://imgs.search.brave.com/SdPT6QcHZeL6ra3eU8PD9vkhD-UcBwAVrXC4FLsiy78/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQw/MzQ0NTg4L3Bob3Rv/L2hvdGVsLXN1aXRl/LWluLXRoZS12aW5l/eWFyZHMtaW4tY2hp/bGUuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWU5TmNHVER2/aUxlcjNFRmR6Z3pr/UVQtNUhhYjBSRG84/aTZNdk01T1U1SVE9" 
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+          <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://imgs.search.brave.com/6jHaN2RGYRwEv0QJK2Lp2A-82QM1UcaQQwMVcojVdvg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/ODA5MTcyMy9lcy9m/b3RvL2FzaXN0ZW50/ZS1kZS1ob3RlbC1j/b24tZXF1aXBhamUt/ZW4tZWwtdmVzdCVD/MyVBRGJ1bG8uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPXhH/Nk1YTHNjdTloaWJH/TGM4TkdPYmVrWWl1/bGJYRndqSVNoa2Rw/Yy1NeHM9"
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="section">
       <h2>Descubre Nuestro Paraíso</h2>
-      <p>Explora nuestras comodidades de lujo y vive una experiencia única en cada rincón de nuestro hotel.</p>
+      <p>
+        Explora nuestras comodidades de lujo y vive una experiencia única en
+        cada rincón de nuestro hotel.
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
-/* Estilo CSS */ 
+/* Estilo CSS */
 * {
   margin: 0;
   padding: 0;
@@ -49,7 +91,7 @@
 }
 
 body {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   background-color: #2c3e50;
   color: #ffffff;
   line-height: 1.6;
@@ -120,7 +162,7 @@ nav a:hover {
 }
 
 .hero-image {
-  background-image: url('descarga.jpg');
+  background-image: url("descarga.jpg");
   background-size: cover;
   background-position: center;
   height: 400px;
@@ -187,5 +229,4 @@ footer p {
 }
 </style>
 
-<script>
-</script>
+<script></script>
