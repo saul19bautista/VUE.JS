59661dffc2fe1505ffd56979bf480a57a2f10fd3
javiern-778 <jcruzespana2@gmail.com>
2025-03-19 22:55:43 -0600

    se agregaron otros 3 bootstrab

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,38 +33,104 @@
     <div class="hero-image">
       <h1>Bienvenido a Hotel Flor del Saber donde el privilegio eres tú</h1>
     </div>
-
-    <div class="row mt-5">
+<!-- Fila de tarjetas -->
+<div class="row mt-5">
+      <!-- Tarjeta 1 -->
       <div class="col-md-4">
         <div class="card" style="width: 18rem">
-          <img src="https://imgs.search.brave.com/MrC3IUGLKbHdv5ng-cIQ6mavCtaHnJ2ncwukVsdSiAE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzYyLzU5Lzcw/LzM2MF9GXzYyNTk3/MDM1X2tKVGtTUFpt/OVAxME9HZnBrV0RY/NWlIbEJ5YXJ1TVVW/LmpwZw" 
-          class="card-img-top" alt="..." />
+          <img
+            src="https://imgs.search.brave.com/MrC3IUGLKbHdv5ng-cIQ6mavCtaHnJ2ncwukVsdSiAE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzYyLzU5Lzcw/LzM2MF9GXzYyNTk3/MDM1X2tKVGtTUFpt/OVAxME9HZnBrV0RY/NWlIbEJ5YXJ1TVVW/LmpwZw"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
           <div class="card-body">
             <p class="card-text text-center">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
             </p>
             <button class="btn btn-warning">Mas Informacion</button>
           </div>
-          <div class="col-md-4">
+        </div>
+      </div>
+
+      <!-- Tarjeta 2 -->
+      <div class="col-md-4">
         <div class="card" style="width: 18rem">
-          <img src="https://imgs.search.brave.com/SdPT6QcHZeL6ra3eU8PD9vkhD-UcBwAVrXC4FLsiy78/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQw/MzQ0NTg4L3Bob3Rv/L2hvdGVsLXN1aXRl/LWluLXRoZS12aW5l/eWFyZHMtaW4tY2hp/bGUuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWU5TmNHVER2/aUxlcjNFRmR6Z3pr/UVQtNUhhYjBSRG84/aTZNdk01T1U1SVE9" 
-          class="card-img-top" alt="..." />
+          <img
+            src="https://imgs.search.brave.com/SdPT6QcHZeL6ra3eU8PD9vkhD-UcBwAVrXC4FLsiy78/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQw/MzQ0NTg4L3Bob3Rv/L2hvdGVsLXN1aXRl/LWluLXRoZS12aW5l/eWFyZHMtaW4tY2hp/bGUuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWU5TmNHVER2/aUxlcjNFRmR6Z3pr/UVQtNUhhYjBSRG84/aTZNdk01T1U1SVE9"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
           <div class="card-body">
             <p class="card-text text-center">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
             </p>
             <button class="btn btn-warning">Mas Informacion</button>
           </div>
-          <div class="col-md-4">
+        </div>
+      </div>
+
+      <!-- Tarjeta 3 -->
+      <div class="col-md-4">
         <div class="card" style="width: 18rem">
-          <img src="https://imgs.search.brave.com/6jHaN2RGYRwEv0QJK2Lp2A-82QM1UcaQQwMVcojVdvg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/ODA5MTcyMy9lcy9m/b3RvL2FzaXN0ZW50/ZS1kZS1ob3RlbC1j/b24tZXF1aXBhamUt/ZW4tZWwtdmVzdCVD/MyVBRGJ1bG8uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPXhH/Nk1YTHNjdTloaWJH/TGM4TkdPYmVrWWl1/bGJYRndqSVNoa2Rw/Yy1NeHM9"
-          class="card-img-top" alt="..." />
+          <img
+            src="https://imgs.search.brave.com/6jHaN2RGYRwEv0QJK2Lp2A-82QM1UcaQQwMVcojVdvg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/ODA5MTcyMy9lcy9m/b3RvL2FzaXN0ZW50/ZS1kZS1ob3RlbC1j/b24tZXF1aXBhamUt/ZW4tZWwtdmVzdCVD/MyVBRGJ1bG8uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPXhH/Nk1YTHNjdTloaWJH/TGM4TkdPYmVrWWl1/bGJYRndqSVNoa2Rw/Yy1NeHM9"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
           <div class="card-body">
             <p class="card-text text-center">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Nueva fila de tarjetas -->
+      <div class="row mt-5">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src="https://imgs.search.brave.com/dZCf5psFWrRhvrf-JWV7k1uJnqEyB1GJO9e7TRaz3nA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE5/MTE5MzA5Mi9lcy9m/b3RvL3JlY2VwY2lv/bmlzdGEtZGVsLWhv/dGVsLXF1ZS1heXVk/YS1hLWxvcy1odSVD/MyVBOXNwZWRlcy1w/YXJhLWVsLWNoZWNr/LWluLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1OWmRHZTNL/T0xaWmhrWnBjLUd5/aFFWTzZNTlpLbWtK/cGlaa1BCbUZmaENn/PQ"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src="https://imgs.search.brave.com/fQgXrUJDdol6zRArpXROJoyUUJNou7Cla5uZdgJpuyg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by91/bWJyZWxsYS13aXRo/LWNoYWlyLWhvdGVs/LXBvb2wtcmVzb3J0/Xzc0MTkwLTQwMDEu/anBnP3NlbXQ9YWlz/X2h5YnJpZA"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src=""
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
             </p>
             <button class="btn btn-warning">Mas Informacion</button>
           </div>
