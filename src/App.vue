5629434fcfc7eebada135f3a0c128cbaa7942e2f
LuzArlethLB <90057862+Luz1727@users.noreply.github.com>
2025-03-19 21:42:40 -0600

    Guardando cambios antes de cambiar de rama

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
   // Se importa el componente HelloWorld desde el archivo correspondiente
-  import HelloWorld from './components/HelloWorld.vue'
+  import Principal from './components/Principal.vue'
 </script>
 
 <template>
   <!-- Se usa el componente HelloWorld y se le pasa una propiedad "msg" con el valor "Vite + Vue" -->
-  <HelloWorld msg="Vite + Vue" />
+  <Principal msg="Vite + Vue" />
 </template>
 
 <style scoped>
