a52a8595e22b8e8e99bf3922dc37898f10ac97bd
javiern-778 <jcruzespana2@gmail.com>
2025-03-25 15:53:47 -0600

    cambios

---
+++ src/components/Bootstrap.vue
@@ -0,0 +1,98 @@
+<template>
+<!-- Fila de tarjetas -->
+<div class="row mt-5">
+      <!-- Tarjeta 1 -->
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src="https://www.cocinayaficiones.com/wp-content/uploads/2023/12/doce-recetas-de-plato-principal-de-lujo-animado.jpg"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Saborea la mejor gastronomía en nuestro restaurante de clase mundial. Platillos exquisitos preparados por chefs reconocidos internacionalmente."
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Tarjeta 2 -->
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src="https://pix8.agoda.net/hotelImages/4424326/-1/9e0be9831f01c8f9f56e8911e19b7f1b.png?ce=0&s=1024x"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Disfruta del máximo confort en nuestras habitaciones de lujo, equipadas con cama king size, minibar y una vista espectacular a la ciudad o al mar."
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Tarjeta 3 -->
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img
+            src="https://laresidenciatarifa.com/wp-content/uploads/2023/05/Spa-La-residencia-puerto-tarifa.webp"
+            class="card-img-top"
+            alt="Imagen tarjeta"
+          />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Relájate en nuestra piscina infinita o disfruta de un masaje en nuestro exclusivo spa, diseñado para brindarte una experiencia de descanso y bienestar".
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Nueva fila de tarjetas -->
+      <div class="row mt-5">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://cdn0.bodas.com.mx/vendor/4683/original/1280/jpg/eduardogaona-august-12-2023-118_5_304683-170421319910611.webp" 
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Organiza tus reuniones, bodas o conferencias en nuestro elegante salón con capacidad para 200 personas y equipo audiovisual de última tecnología."
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://www.cmdsport.com/app/uploads/2024/11/inner-flow-sala.jpg" 
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Mantente en forma en nuestro gimnasio de última generación o disfruta de actividades como yoga, pilates y entrenamiento personalizado."
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem">
+          <img src="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/12/78/0b/c9.jpg"
+          class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text text-center">
+              "Descubre experiencias inolvidables con nuestras excursiones guiadas, deportes acuáticos y espectáculos nocturnos para toda la familia".
+            </p>
+            <button class="btn btn-warning">Mas Informacion</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
